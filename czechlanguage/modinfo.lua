be4571ab2c81e7c9f30b89b2fa9a5eb77a61be34
KappaLabs <fojjta@gmail.com>
2013-09-13 12:56:07 +0200

    Přidány soubory pro možnost testování stability verze.

---
+++ czechlanguage/modinfo.lua
@@ -0,0 +1,13 @@
+name = "Czech Language Pack"
+description = "Balicek s ceskym prekladem (67%)."
+author = "Kappa a Keebleec"
+version = "1.67.0"
+
+-- http://forums.kleientertainment.com/index.php?/files/file/202-sample-mods/
+-- <-- /files/file/202-sample-mods/
+forumthread = "/files/file/202-sample-mods/"
+
+api_version = 4
+
+icon_atlas = "czechlanguage.xml"
+icon = "czechlanguage.tex"
---
+++ czechlanguage/modmain.lua
@@ -0,0 +1,26 @@
+-- Nacteni upravenych textur do hry.
+Assets = {
+	Asset("ATLAS", "bigportraits/locked.xml"),
+	Asset("IMAGE", "bigportraits/locked.tex"),
+
+	Asset("ATLAS", "images/customisation.xml"),
+	Asset("IMAGE", "images/customisation.tex"),
+
+--	Asset("ATLAS", "images/ui.xml"),
+--	Asset("IMAGE", "images/ui.tex"),
+}
+
+GLOBAL.STRINGS.DFV_LANG = "CS"
+GLOBAL.STRINGS.DFV_HUNGER = "Hlad"
+GLOBAL.STRINGS.DFV_HEALTH = "Zdravi"
+GLOBAL.STRINGS.DFV_SANITY = "Pricetnost"
+GLOBAL.STRINGS.DFV_SPOILSOON = "Brzy se skazi"
+GLOBAL.STRINGS.DFV_SPOILIN = "Skazi se za"
+GLOBAL.STRINGS.DFV_SPOILDAY = "dni"
+
+-- V okamziku podpory formatovani UTF-8 odkomentovat prvni a zakomentovat druhou metodu LoadPOFile
+
+-- LoadPOFile("czech.po", "CS")
+LoadPOFile("czech_ascii.po", "CS")
+
+-- Informace o tvorbe prekladu: http://forums.kleientertainment.com/index.php?/topic/10292-creating-a-translation-using-the-po-format/
