ea4967597b4f2ec79461fb82fc7bef38f133e34d
KappaLabs <fojjta@gmail.com>
2013-10-14 21:55:32 +0200

    [BUGFIX] Pád při stisku Alt
    Opravena chyba způsobená absencí funkce pro zaokrouhlování v souboru itemtile.lua.

--- czechlanguage/scripts/widgets/itemtile.lua
+++ czechlanguage/scripts/widgets/itemtile.lua
@@ -276,6 +276,13 @@ function CZTGetReplacementIte(text, part)
 	else return ""
 	end
 end
+
+function round(x)
+	if x%2 ~= 0.5 then
+		return math.floor(x+0.5)
+	end
+	return x-0.5
+end
 --### <EO> MOD CzechTranslationFeature <--
 
 function ItemTile:OnGainFocus()
