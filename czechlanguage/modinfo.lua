c2a811d06067a836b8a73cd7716d62b8b6134c7e
KappaLabs <fojjta@gmail.com>
2013-10-28 13:57:03 +0100

    [TASK] Korektury překladů Maxwella

--- czechlanguage/modinfo.lua
+++ czechlanguage/modinfo.lua
@@ -1,7 +1,7 @@
 name = "Czech Language Pack"
 description = "Balicek s cestinou do hry (89%)."
 author = "Kappa a Keebleec"
-version = "1.89"
+version = "1.89.1"
 
 -- http://forums.kleientertainment.com/index.php?/files/file/202-sample-mods/
 -- <-- /files/file/202-sample-mods/
@@ -13,4 +13,4 @@ icon_atlas = "czechlanguage.xml"
 icon = "czechlanguage.tex"
 
 -- Specify the priority
-priority=2
+priority=5
