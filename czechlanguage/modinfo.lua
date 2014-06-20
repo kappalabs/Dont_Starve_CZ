5ca57bf640b3633259123f3c9d9333b74f9bccd7
KappaLabs <fojjta@gmail.com>
2014-06-20 22:15:18 +0200

    [BUGFIX] Update dat  - opraveno zobrazování položek při generování světa ad.  - update API verze a související nastavení  - přidání překladu featury pro informace o jídle

--- czechlanguage/modinfo.lua
+++ czechlanguage/modinfo.lua
@@ -1,13 +1,16 @@
 name = "Czech Language Pack"
+id = "czechlanguagepack"
 description = "Balicek s cestinou do hry."
 author = "Kappa a KeeBLeeC"
-version = "2.0.8"
+version = "2.1.0"
 
 -- http://forums.kleientertainment.com/index.php?/files/file/202-sample-mods/
 -- <-- /files/file/202-sample-mods/
 forumthread = "/files/file/202-sample-mods/"
 
-api_version = 5
+api_version = 6
+dont_starve_compatible = true
+reign_of_giants_compatible = true
 
 icon_atlas = "czechlanguage.xml"
 icon = "czechlanguage.tex"
