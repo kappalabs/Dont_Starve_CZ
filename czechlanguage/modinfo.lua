5d1a980423bd6fedd40b7824ea74ddb6d456e469
KappaLabs <fojjta@gmail.com>
2016-12-26 20:20:53 +0100

    [TASK] Překlad 42%
     - další překlady menu výroby
     - opravy chyb

--- czechlanguage/modinfo.lua
+++ czechlanguage/modinfo.lua
@@ -1,7 +1,7 @@
 
 name = "Czech Language Pack"
 id = "czechlanguagepack"
-version = "2.3.2"
+version = "2.3.3"
 desc = {en = "Czech translation of the game.\n\nVersion: "..version,
 		    cz = "\nBalíček s češtinou do hry.\n\nVerze: "..version}
 description = desc[language] or desc["en"]
